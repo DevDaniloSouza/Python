61dc686e8369cdcc1fe90ca99e332ee3264a02af
Danilo P. de Souza <danilopursino27@gmail.com>
2026-05-07 21:23:57 -0300

    Rich

--- Python_POO/Rich/rich001.py
+++ Python_POO/Rich/rich001.py
@@ -1,3 +1,4 @@
 from rich import print
 
 print('[red]Mensagem de erro aleatória![/]')
+print('Olá pequeno [bold blue on white]Gafanhoto![/] :vulcan_salute:')
---
+++ Python_POO/Rich/rich002.py
@@ -0,0 +1,6 @@
+from rich import print
+from rich.panel import Panel
+
+caixa = Panel('Esse é meu painel de exemplo para o Curso em Vídeo. ERRO 404', title='ERRO', style='red', width=50)
+
+print(caixa)
