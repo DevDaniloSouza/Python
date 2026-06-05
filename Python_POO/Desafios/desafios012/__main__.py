607a5b075ab1babda54d3608e69ff2ce9eb6bfcd
Danilo P. de Souza <danilopursino27@gmail.com>
2026-06-04 21:00:51 -0300

    Desafios Python

--- Python_POO/Desafios/desafios012/__main__.py
+++ Python_POO/Desafios/desafios012/__main__.py
@@ -1,5 +1,17 @@
+from classes import *
+
 def main():
-    pass
+    p1 = Guerreiro("Kratos", 3000)
+    p2 = Mago("Merlim", 2000)
+
+    p1.atacar(p2, 1000)
+    p2.atacar(p1, 1500)
+
+    p1.curar()
+    p2.curar()
+    
+    p1.atacar(p2, 1000)
+    p2.atacar(p1, 1500)
 
 if __name__ == "__main__":
     main()
--- Python_POO/Desafios/desafios012/classes.py
+++ Python_POO/Desafios/desafios012/classes.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from rich import print
+import random
 
 class Personagem(ABC):
     def __init__(self, nome, vida):
@@ -7,11 +9,15 @@ class Personagem(ABC):
         self.vida = vida
         self.golpes = []
 
-    def atacar(self, alvo, forca):
-        pass
+    def atacar(self, alvo:Personagem, forca):
+        dano = random.randint(1, forca)
+        golpe = random.randint(0, 2)
+        print(f"[green]{self.nome}[/]({self.vida}) atacou [red]{alvo.nome}[/]({alvo.vida}) com [blue]{self.golpes[golpe]}[/] de força {forca}.")
+        alvo.receber_dano(dano)
 
-    def receber_dano(self):
-        pass
+    def receber_dano(self, dano):
+        self.vida -= dano
+        print(f"[blue]{self.nome}[/] recebeu [red]dano de {dano}[/]!")
 
     @abstractmethod
     def curar(self):
@@ -21,17 +27,21 @@ class Personagem(ABC):
 class Guerreiro(Personagem):
     def __init__(self, nome, vida):
         super().__init__(nome, vida)
-        self.golpes = []
+        self.golpes = ["Soco", "Pulo Giratório", "Chute"]
 
     def curar(self):
-        pass
+        cura = random.randint(0, 100)
+        self.vida += cura
+        print(f"[blue]{self.nome}[/] tomou uma poção de cura e [green]recuperou {cura} pontos[/] de vida")
 
 
 class Mago(Personagem):
     def __init__(self, nome, vida):
         super().__init__(nome, vida)
-        self.golpes = []
+        self.golpes = ["Mísseis Mágicos", "Bola de Fogo", "Cajado"]
 
     def curar(self):
-        pass
+        cura = random.randint(0, 100)
+        self.vida += cura
+        print(f"[blue]{self.nome}[/] fez uma magia de cura e [green]recuperou {cura} pontos[/] de vida")
 
