22612a7849b7f1808d984baa73e5b2a3710d721e
Danilo P. de Souza <121466843+DevDaniloSouza@users.noreply.github.com>
2026-06-05 16:42:41 -0300

    Implement freight table display in main function
    Add a table to display freight calculations for different vehicle types.

--- Python_POO/Desafios/desafios010/__main__.py
+++ Python_POO/Desafios/desafios010/__main__.py
@@ -1,10 +1,24 @@
 from classes import Moto, Caminhao, Drone
+from rich import print
+from rich.table import Table
 
 def main():
     dist = 10
 
     entrega = Drone(dist)
     print(f'Frete de {type(entrega).__name__} em {dist}Km = {entrega.calcular_frete()}.')
+
+    viagem = [Moto(dist), Caminhao(dist), Drone(dist)]
+
+    tabela = Table(title='Tabela de Fretes')
+    tabela.add_column('Distância')
+    tabela.add_column('Tipo')
+    tabela.add_column('Frete')
+    
+    for item in viagem:
+        tabela.add_row(f'{dist}', f'{type(item).__name__}', f'{item.calcular_frete()}'
+
+    print(tabela)
 
 if __name__ == "__main__":
     main()
